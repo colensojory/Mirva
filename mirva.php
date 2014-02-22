31f85f45a40d4aa33828c32cfb2e317200efb44b
Oliver Jory <ollie.t.jory@gmail.com>
2014-02-22 13:05:14 +0100

    Initial Commit - mirva.php 'Standard Plugin Information'

---
+++ mirva.php
@@ -0,0 +1,11 @@
+<?php 
+    /*
+    Plugin Name: Mirva
+    Plugin URI: http://colensojory.github.io/mirva
+    Description: Mirva is a Portfolio custom post type utilising the WordPress Media Library.
+    Author: Colenso Jory
+    Version: 0.1
+    Author URI: http://colensojory.github.io
+    */
+?>
+
